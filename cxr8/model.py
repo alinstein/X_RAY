14f9520a885024d47919ff117c429122c2acfe13
alinstein <alinsteinjose@gmail.com>
2020-12-25 15:17:29 -0800

    cleaned code, eval function,
    Signed-off-by: alinstein <alinsteinjose@gmail.com>

--- cxr8/model.py
+++ cxr8/model.py
@@ -11,18 +11,16 @@ class DenseNet121(nn.Module):
     The architecture of our model is the same as standard DenseNet121
     except the classifier layer which has an additional sigmoid function.
     """
-    def __init__(self, out_size):
+
+    def __init__(self, cfg):
         super(DenseNet121, self).__init__()
         self.densenet121 = models.densenet121(pretrained=True)
         num_ftrs = self.densenet121.classifier.in_features
         self.densenet121.classifier = nn.Sequential(
-            nn.Linear(num_ftrs, out_size),
+            nn.Linear(num_ftrs,  cfg.num_classes),
             nn.Sigmoid()
         )
-
     def forward(self, x):
-#        print(x.shape)
-        x = torch.cat((x, x, x), dim=1)
         x = self.densenet121(x)
         return x
         
